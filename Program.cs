9a78b0ff50b6ef0ee52926e4821b9ae40eaa5c75
emandere <emandere@gmail.com>
2020-04-26 03:56:48 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace forex_app_trader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
